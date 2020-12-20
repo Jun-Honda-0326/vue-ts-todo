a315f2af7feccce8cfe438f20fe50c2d117376bb
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-12-20 16:33:24 +0900

    first commit

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <form>
+    <input type="text" v-model="newItem">
+    <p>{{ newItem }}</p>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class TodoForm extends Vue{
+    newItem = ''
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <p>{{ greetText }}</p>
+    <TodoForm></TodoForm>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TodoForm from "@/components/TodoForm.vue";
+
+@Component({
+  components: {
+    TodoForm
+  }
+})
+export default class Home extends Vue {
+  public greetText = "Hello Vue.js";
+}
+</script>
