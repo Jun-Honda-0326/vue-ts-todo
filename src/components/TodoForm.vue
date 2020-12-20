cdade4be1d9a4b41ae0f14bed0d92eb979277e5b
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-12-20 16:37:31 +0900

    create add btn

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,15 +1,20 @@
 <template>
   <form>
     <input type="text" v-model="newItem">
+    <button @click="addItem">Todo追加</button>
     <p>{{ newItem }}</p>
   </form>
 </template>
 
 <script lang="ts">
-import { Component, Vue} from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class TodoForm extends Vue{
-    newItem = ''
+export default class TodoForm extends Vue {
+  newItem = "";
+
+  addItem() {
+    alert();
+  }
 }
 </script>
